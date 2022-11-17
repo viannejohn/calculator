a4f480f5ece6d9712424501f5f5fcd2030dbf30a
viannejohn <vianneloro@gmail.com>
2022-11-17 19:19:27 +0300

    MAIN

--- my_calculator/lib/main.dart
+++ my_calculator/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unnecessary_new, avoid_unnecessary_containers, use_key_in_widget_constructors
+// ignore_for_file: unnecessary_new, avoid_unnecessary_containers, use_key_in_widget_constructors, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
